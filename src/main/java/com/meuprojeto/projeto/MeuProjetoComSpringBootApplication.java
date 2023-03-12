9b85223ec1b585960444fd89504f44ecd56eef0f
Marcos Moura <marcosmoura201@hotmail.com>
2023-03-12 17:04:34 -0300

    Criando meu Projeto com SpringBoot

---
+++ src/main/java/com/meuprojeto/projeto/MeuProjetoComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoComSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meuprojeto/projeto/MeuProjetoComSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoComSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
